7dd434b0aba42d8cef006f5da6bf0ad5aadf4759
Rpsxxxxxxn <keito0122.y@gmail.com>
2023-12-22 15:06:15 +0900

    ミドルウェアの更新

--- src/Middleware/AuthMiddleware.ts
+++ src/Middleware/AuthMiddleware.ts
@@ -4,14 +4,14 @@ import admin from '../FirebaseAdmin';
 const middleware = async (req: Request, res: Response, next: NextFunction) => {
   const token = req.headers.authorization;
   if (!token) {
-    return res.status(401).send('Authorization token missing');
+    return res.status(401).send('Tokenがありません');
   }
   try {
     const decodedToken = await admin.auth().verifyIdToken(token);
     (req as any).user = decodedToken;
     next();
   } catch (error) {
-    return res.status(401).send('Invalid or expired token');
+    return res.status(401).send('Tokenが無効です');
   }
 }
 
